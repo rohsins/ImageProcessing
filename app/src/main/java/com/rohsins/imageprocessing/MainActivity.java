2b9ae0fd46f38027e52e7762d4b4f3d827d57e64
Rohit Singh <rohit.singh@rts.com.np>
2016-05-31 13:51:38 +0545

    take photo and video working fine

--- app/src/main/java/com/rohsins/imageprocessing/MainActivity.java
+++ app/src/main/java/com/rohsins/imageprocessing/MainActivity.java
@@ -243,10 +243,6 @@ public class MainActivity extends Activity {
             };
 
 
-
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
